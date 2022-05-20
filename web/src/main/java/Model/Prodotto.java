99d3f08fb8dc2c49102edddfcabb1a871734144b
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-05-20 12:24:46 +0200

    Creazione doRetriveByType

--- web/src/main/java/Model/Prodotto.java
+++ web/src/main/java/Model/Prodotto.java
@@ -6,6 +6,21 @@ public class Prodotto {
     private String modello;
     private double prezzo;
     private String type;
+    private int wattaggio;
+    private int frequenza;
+    private int N_CORE;
+    private int N_RAM;
+    private int N_USB;
+    private int N_PCI;
+    private int MBs;
+    private int VRAM;
+    private int N_WATT;
+    private int W_CPU;
+    private short FormaMobo;
+    private String url;
+    private String Descrizione;
+
+
     public Prodotto() {
 
     }
@@ -16,6 +31,110 @@ public class Prodotto {
         this.modello = modello;
         this.prezzo = prezzo;
         this.type = type;
+    }
+
+    public int getWattaggio() {
+        return wattaggio;
+    }
+
+    public void setWattaggio(int wattaggio) {
+        this.wattaggio = wattaggio;
+    }
+
+    public int getFrequenza() {
+        return frequenza;
+    }
+
+    public void setFrequenza(int frequenza) {
+        this.frequenza = frequenza;
+    }
+
+    public int getN_CORE() {
+        return N_CORE;
+    }
+
+    public void setN_CORE(int n_CORE) {
+        N_CORE = n_CORE;
+    }
+
+    public int getN_RAM() {
+        return N_RAM;
+    }
+
+    public void setN_RAM(int n_RAM) {
+        N_RAM = n_RAM;
+    }
+
+    public int getN_USB() {
+        return N_USB;
+    }
+
+    public void setN_USB(int n_USB) {
+        N_USB = n_USB;
+    }
+
+    public int getN_PCI() {
+        return N_PCI;
+    }
+
+    public void setN_PCI(int n_PCI) {
+        N_PCI = n_PCI;
+    }
+
+    public int getMBs() {
+        return MBs;
+    }
+
+    public void setMBs(int MBs) {
+        this.MBs = MBs;
+    }
+
+    public int getVRAM() {
+        return VRAM;
+    }
+
+    public void setVRAM(int VRAM) {
+        this.VRAM = VRAM;
+    }
+
+    public int getN_WATT() {
+        return N_WATT;
+    }
+
+    public void setN_WATT(int n_WATT) {
+        N_WATT = n_WATT;
+    }
+
+    public int getW_CPU() {
+        return W_CPU;
+    }
+
+    public void setW_CPU(int w_CPU) {
+        W_CPU = w_CPU;
+    }
+
+    public short getFormaMobo() {
+        return FormaMobo;
+    }
+
+    public void setFormaMobo(short formaMobo) {
+        FormaMobo = formaMobo;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescrizione() {
+        return Descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        Descrizione = descrizione;
     }
 
     public String getMarca() {
--- web/src/main/java/Model/ProdottoDAO.java
+++ web/src/main/java/Model/ProdottoDAO.java
@@ -1,15 +1,12 @@
 package Model;
 
 import java.lang.reflect.Type;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class ProdottoDAO {
+public class ProdottoDAO<E> {
     public List<Prodotto> doRetriveAll() throws SQLException {
         Connection conn = ConPool.getConnection();
         PreparedStatement ps = conn.prepareStatement("SELECT * FROM Pezzo");
@@ -24,6 +21,27 @@ public class ProdottoDAO {
 
     public List<Prodotto> doRetriveByType(String type) throws SQLException {
         Connection conn = ConPool.getConnection();
-        PreparedStatement ps = conn.prepareStatement("SELECT * FROM Pezzo WHERE ");
+        PreparedStatement ps = conn.prepareStatement("SELECT * FROM Pezzo WHERE tipo = ?");
+        ps.setString(1, type);
+        ResultSet rs = ps.executeQuery();
+        List<Prodotto> list = new ArrayList<Prodotto>();
+        while(rs.next()){
+            Prodotto p = new Prodotto(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4), rs.getString(6));
+            p.setWattaggio(rs.getInt(5));
+            p.setFrequenza(rs.getInt(7));
+            p.setN_CORE(rs.getInt(8));
+            p.setN_RAM(rs.getInt(9));
+            p.setN_USB(rs.getInt(10));
+            p.setN_PCI(rs.getInt(11));
+            p.setMBs(rs.getInt(12));
+            p.setVRAM(rs.getInt(13));
+            p.setN_WATT(rs.getInt(14));
+            p.setW_CPU(rs.getInt(15));
+            p.setFormaMobo(rs.getShort(16));
+            p.setUrl(rs.getString(17));
+            p.setDescrizione(rs.getString(18));
+            list.add(p);
+        }
+        return list;
     }
 }
