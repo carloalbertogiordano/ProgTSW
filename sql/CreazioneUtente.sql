00ce373f096409d8071602f0a086fa521bd34c3a
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-05-20 11:23:42 +0200

    Modifica psw DB

--- sql/CreazioneUtente.sql
+++ sql/CreazioneUtente.sql
@@ -1,4 +1,4 @@
 USE Ecommerce;
-CREATE USER 'user1'@'localhost' IDENTIFIED BY 'password1';
+CREATE USER 'user1'@'localhost' IDENTIFIED BY 'VoW!S$!h3udC#W';
 grant all on Ecommerce.* to 'user1'@'localhost';
 flush privileges;
