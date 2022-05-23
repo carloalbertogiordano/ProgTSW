f5071307445605cdd1094f3fb1746959b6e695f4
Cvlmtt <97099847+Cvlmtt@users.noreply.github.com>
2022-05-23 21:12:35 +0200

    gestione carrello

--- web/src/main/java/Model/Carrello_/CarrelloDAO.java
+++ web/src/main/java/Model/Carrello_/CarrelloDAO.java
@@ -18,7 +18,7 @@ public class CarrelloDAO {
     public ArrayList<Integer> doRetriveByMailCliente(String mail) throws SQLException {
         Connection con = ConPool.getConnection();
         Statement stmt = (Statement) con.createStatement();
-        PreparedStatement pdstmt = con.prepareStatement("SELECT PezzoID FROM Comporre WHERE CarrelloCod IN (SELECT Mail FROM Ordine WHERE ClienteID = ? AND Evaso != true);");
+        PreparedStatement pdstmt = con.prepareStatement("SELECT PezzoID FROM Comporre WHERE CarrelloCod IN (SELECT Mail FROM Ordine WHERE ClienteMail = ? AND Evaso != true);");
         pdstmt.setString(1, mail);
         ResultSet rs = pdstmt.executeQuery();
         ArrayList<Integer> prodotti = new ArrayList<Integer>();
