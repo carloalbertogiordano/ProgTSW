88985d0c44c885c54298a3cfbb86a87e34ee6b4c
carloalbertogiordano <carloalbertogiordano@hotmail.com>
2022-07-19 18:22:49 +0200

    BugFix: Modificando manualmente l'imput type era possibile comprare più prodotti di quanti disponibili

--- web/src/main/webapp/js/carrello.js
+++ web/src/main/webapp/js/carrello.js
@@ -6,14 +6,21 @@ $(document).ready(function(){
     let oldQuant = saveOldQuant();
     $('#submit').click(function(){
         let id = $(this).parent().attr("id");
-        let quant = $('#quantOf'+id).val();
-        console.log(quant+' Id'+id+' oldQuant'+oldQuant);
-        $.ajax({
-            url: 'modQuantCartDB',
-            type: 'GET',
-            data: {attr_id: id, attr_newQuant: quant, attr_OldQuant: oldQuant
-            },
-        });
-        oldQuant = quant;
+        let quantOf = $('#quantOf'+id);
+        let quant = Number(quantOf.val());
+        if(Number(quantOf.attr("max"))>=quant) {
+            console.log(quant + ' Id' + id + ' oldQuant' + oldQuant);
+            $.ajax({
+                url: 'modQuantCartDB',
+                type: 'GET',
+                data: {
+                    attr_id: id, attr_newQuant: quant, attr_OldQuant: oldQuant
+                },
+            });
+            oldQuant = quant;
+        }
+        else{
+            quantOf.val(oldQuant);
+        }
     });
 });
